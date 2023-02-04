bc11b41967c1edf0f6e89d9e14db32602d93aff8
nwahidm <nwahidm@gmail.com>
2023-02-04 21:14:21 +0700

    Add profile page

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,15 +1,14 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Home from './screen/home';
 import LoginRegister from './screen/LoginRegister';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Games from './src/screens/Games/Games';
 import Categories from './src/screens/Categories/Categories'
+import Play from './src/screens/Play';
+import Profile from './screen/Profile';
 import 'react-native-gesture-handler';
 import 'react-native-reanimated'
-import Play from './src/screens/Play';
+
 
 export default function App() {
   const Stack = createNativeStackNavigator()
@@ -21,6 +20,7 @@ export default function App() {
         <Stack.Screen name='categories' component={Categories} options={{headerShown: false}} />
         <Stack.Screen name='games' component={Games} options={{headerShown: false}} />
         <Stack.Screen name='plays' component={Play} options={{headerShown: false}} />
+        <Stack.Screen name='Profile' component={Profile} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
